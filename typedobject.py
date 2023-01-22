839f916256b5bc41ab5706fe8f42c698bbc62c44
jessehl <j.loor@hotmail.com>
2023-01-22 17:26:33 +0100

    simplify object and plugin

--- typedobject.py
+++ typedobject.py
@@ -1,25 +1,21 @@
 from __future__ import annotations
-from typing import Optional
+from typing import Optional, Union, Dict, Any
 from itertools import chain
 
 class Object:
     
     def __init__(self, *args: Optional[Object], **kwargs):
-        attrs_to_set = {k:v for k,v in chain(
+        for k,v in dict(chain(
             chain.from_iterable((arg.__dict__.items() for arg in args if arg)),
             kwargs.items()
-        )}
-        for k,v in attrs_to_set.items():
+        )).items():
             self.__setattr__(k,v) 
 
     def __repr__(self) -> str:
-        values = (key.strip("'") + ' = ' + value.__repr__() for key, value in self.__dict__.items())
-        return  f"({', '.join(values)})"
+        quote = "'"
+        return  f" ({', '.join(key.strip(quote) + '=' + value.__repr__() for key, value in self.__dict__.items())}) "
 
-
-g = Object(Object(a = 1), None, a = '1', b = 2)
-
-
-
-
-
+def type_check() -> None:
+    a = Object(a = 1)
+    
+    Object(a).a + 100 
--- typedobjectplugin.py
+++ typedobjectplugin.py
@@ -1,68 +1,56 @@
 from __future__ import annotations
-from typing import Callable, TypeVar
-from mypy.nodes import (
-    Var, SymbolTableNode, MDEF
-)
-
-from mypy.types import Instance, NoneType
+from typing import Callable
+from mypy.nodes import Var, SymbolTableNode, MDEF
+from mypy.types import Instance
 from mypy.types import Type, Instance
 from mypy.plugin import Plugin, FunctionContext
 from itertools import chain
-from typedobject import Object
-
-
-class Bar:
-    def visit_instance(self, ins):
-        print(ins)
 
 class TypedObjectPlugin(Plugin):
-    """A plugin to make MyPy understand TypedObjects."""
+    """ A plugin to make MyPy understand TypedObjects."""
     def get_function_hook(self, fullname: str) -> Callable[[FunctionContext], Type] | None:
         if fullname == 'typedobject.Object':
-            return add_input_args_to_class_hook
+            return add_input_args_to_class
         return None
 
-def add_input_args_to_class_hook(ctx: FunctionContext):
+
+def add_input_args_to_class(ctx: FunctionContext):
     assert isinstance(ctx.default_return_type, Instance)
 
     # Fetch argument name-type pairs (both args and kwargs).
     input_args = chain(zip(chain.from_iterable(ctx.arg_names), chain.from_iterable(ctx.arg_types)))
-    names_types = list((name, type) for name, type in input_args if not isinstance(type, NoneType))
 
+    # TODO: how is it possible that nested object assignment (i.e. 'a = Object(a = 1); b = Object(a)') is typed properly?
+    # The types of 'a' are never assigned to the created object, it seems. 
+
+    # TODO: add support for inferring return types when args contains a Union of Objects.  
+
+    # args = ctx.arg_types[0]
+    # if args:
+
+     #   items = args[0].type.names.items()
+      #  for key, item in items:
+       #     print(key)
+        #    print(item)
+            
+    info = ctx.default_return_type.type
+    def add_field(name: str, type: Type):
+        var =               Var(name, type)
+        var.info =          info
+        var._fullname =     f"{info.fullname}.{var.name}"
+        info.names[name] =  SymbolTableNode(MDEF, var)
+        return (type, name)
+
+    test = [add_field(str(name), type) for name, type in input_args]
     print("here")
-    # TODO: Get all attributes from 'args' (i.e. from other TypedObjects).
-    try:
-        bar = [(name, Instance(type, []).type.slots) for name, type in names_types]
-        print('bar', bar)
-    except Exception as e:
-        print(e)
+    print(test)
 
-    print('names_types', names_types)
-
-    info = ctx.default_return_type.type 
-
-    def add_field(
-        var: Var, is_initialized_in_class: bool = False, is_property: bool = False
-    ) -> None:
-        var.info = info
-        var.is_initialized_in_class = is_initialized_in_class
-        var.is_property = is_property
-        var._fullname = f"{info.fullname}.{var.name}"
-        info.names[var.name] = SymbolTableNode(MDEF, var)
-
-    fields = [Var(str(name), typ) for name, typ in names_types]
-    for var in fields:
-        add_field(var, True, True)
-
-    my_instance = Instance(ctx.default_return_type.type, [],
-                line=ctx.default_return_type.line,
-                column=ctx.default_return_type.column)
-
-    return my_instance
-
-
-
-
+    return Instance(
+        typ =       ctx.default_return_type.type, 
+        args =      [],
+        line =      ctx.default_return_type.line,
+        column =    ctx.default_return_type.column
+    )
 
 
 def plugin(version: str):
