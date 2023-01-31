247afce03c5087167b97265b5408cec4d50f2496
jessehl <j.loor@hotmail.com>
2023-01-31 16:54:14 +0100

    clean

--- typedobject.py
+++ typedobject.py
@@ -1,44 +1,19 @@
 from __future__ import annotations
-from typing import Callable, Dict, Any, Protocol
 from itertools import chain
 
 
-class _Protocol(Protocol):
-    __dict__: Dict[str, Any]
+
 
 class Object:
-
-    def __init__(self, *args: _Protocol, **kwargs):
-        for k,v in dict(chain(
-            chain.from_iterable((arg.__dict__.items() for arg in args if arg)),
-            kwargs.items()
-        )).items():
-            self.__setattr__(k,v)
-            
-    def __dir__(self):
-        return list(self.__dict__.keys())
-
+    def __init__(self, *args: Object, **kwargs) -> None:
+        self.__dict__.update(
+            chain.from_iterable((vars(arg).items() for arg in args if arg)),
+            **kwargs
+        )
+        
     def __repr__(self) -> str:
         quote = "'"
-        return  f" ({', '.join(key.strip(quote) + '=' + value.__repr__() for key, value in self.__dict__.items())}) "
+        return  f"({', '.join(key.strip(quote) + '=' + value.__repr__() for key, value in self.__dict__.items())})"
 
 
 
-class Bar(_Protocol):
-    a: int
-    b: Callable[[], None]
-    c: str
-
-
-def type_check(a: Bar):
-    g = Object(a)
-    g
-    return Object(g).a + 100
-
-a = type_check(Object(a = '3'))
-
-
-
-
-
-    
--- typedobjectplugin.py
+++ typedobjectplugin.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-from typing import Callable
-from mypy.nodes import Var, SymbolTableNode, MDEF, TypeInfo
+from typing import Callable, Type
+from mypy.nodes import Var, SymbolTableNode, MDEF
 from mypy.types import Instance
-from mypy.types import Type, Instance
+from mypy.types import Type
 from mypy.plugin import Plugin, FunctionContext
 from itertools import chain
+
 
 class TypedObjectPlugin(Plugin):
     """ A plugin to make MyPy understand TypedObjects."""
@@ -17,31 +18,39 @@ class TypedObjectPlugin(Plugin):
 def add_input_args_to_class(ctx: FunctionContext):
     assert isinstance(ctx.default_return_type, Instance)
 
+
     # Fetch args types.
     objects = (_ for _ in ctx.arg_types[0] if isinstance(_, Instance))
     args_of_objects = chain.from_iterable(_.type.names.items() for _ in objects)
-    args_types = ((arg, node.type) for arg, node in args_of_objects if not node.type is None)
+    args = ((arg, node.type) for arg, node in args_of_objects if not node.type is None)
 
     # Fetch kwargs types. 
-    kwargs_types = zip(chain.from_iterable(ctx.arg_names[1:]), chain.from_iterable(ctx.arg_types[1:]))
+    kwargs = zip(chain.from_iterable(ctx.arg_names[1:]), chain.from_iterable(ctx.arg_types[1:]))
+
+    # Dedupe fields. 
+    all_fields = dict(chain(args, kwargs))
+
 
     # TODO: add support for inferring return types when args contains a Union of Objects.  
 
-    
-    info = ctx.default_return_type.type
+    # TODO: Object() now only has one Symbol Table entry, so we're overwriting this entry when modifing the return type. 
+
+    info = ctx.default_return_type.copy_modified(args = [v for k,v in all_fields.items()]).type
+
+    info._fullname = str(all_fields)
     def add_field(name: str, type: Type):
         var =               Var(name, type)
         var.info =          info
         var._fullname =     f"{info.fullname}.{var.name}"
         info.names[name] =  SymbolTableNode(MDEF, var)
         return (type, name)
-
-    all_fields = dict(chain(args_types, kwargs_types))
-    print([add_field(str(name), type) for name, type in all_fields.items()])
     
+    print([add_field(str(name), type) for name, type in all_fields.items()])
+
+
     return Instance(
-        typ =       ctx.default_return_type.type, 
-        args =      [],
+        typ =       info,
+        args =      list(all_fields.values()),
         line =      ctx.default_return_type.line,
         column =    ctx.default_return_type.column
     )
